a8253d282ff98745d20e1a585cf5487eeb7bfe9d
DDiachenko6559 <153743209+DDiachenko6559@users.noreply.github.com>
2024-05-31 22:14:49 +0300

    load more

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,20 +1,23 @@
-export const getImages = searchCategory => {
-  const apiKey = '44070082-fd53ee46a47d37a3fc9d7b56d';
+'use strict';
+
+import axios from 'axios';
+
+export const getImages = async (searchCategory, page) => {
   const BASE_URL = 'https://pixabay.com/api/';
-  const params = new URLSearchParams({
-    key: apiKey,
-    q: searchCategory,
-    image_type: 'photo',
-    orientation: 'horizontal',
-    safesearch: true,
-  });
-
-  const url = `${BASE_URL}?${params}`;
-
-  return fetch(url).then(res => {
-    if (!res.ok) {
-      throw new Error(res.statusText);
-    }
-    return res.json();
-  });
+  try {
+    const response = await axios.get(BASE_URL, {
+      params: {
+        key: '44070082-fd53ee46a47d37a3fc9d7b56d',
+        q: searchCategory,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: true,
+        per_page: '15',
+        page: page,
+      },
+    });
+    return response.data;
+  } catch (error) {
+    throw new Error(error.message);
+  }
 };
--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
+'use strict';
+
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 import SimpleLightbox from 'simplelightbox';
@@ -8,49 +10,145 @@ import { GalleryItem } from './js/render-functions.js';
 
 const queryEl = {
   galleryEl: document.querySelector(`.gallery-list`),
-  loader: document.querySelector(`.loader`),
+  galleryItem: document.querySelector(`.gallery-item`),
   searchForm: document.querySelector(`.form`),
+  searchBtn: document.querySelector('.search-btn'),
+  loader: document.querySelector(`.loader`),
+  loadBtn: document.querySelector(`.load-more-btn`),
 };
+
+let page = 1;
+let totalHits = 0;
+let globalSearchCategory;
+
+queryEl.searchForm.addEventListener(`submit`, galleryOn);
+queryEl.loadBtn.addEventListener('click', handleLoad);
+
+async function galleryOn(e) {
+  e.preventDefault();
+  loaderShow();
+
+  queryEl.galleryEl.innerHTML = '';
+
+  const searchCategory = e.target.elements.search.value.trim();
+  globalSearchCategory = searchCategory;
+  if (!searchCategory) {
+    queryEl.galleryEl.innerHTML = '';
+    e.target.reset();
+    return;
+  }
+
+  try {
+    const imagesData = await getImages(searchCategory, (page = 1));
+    if (imagesData.hits.length) {
+      queryEl.galleryEl.innerHTML = GalleryItem(imagesData.hits);
+      lightbox.refresh();
+      totalHits = Math.ceil(imagesData.totalHits / 15);
+      if (page < totalHits) {
+        visibilityLoad();
+      } else {
+        hiddenLoad();
+        iziToast.error({
+          message: "We're sorry, but you've reached the end of search results.",
+          backgroundColor: '#EF4040',
+          theme: 'dark',
+          maxWidth: 354,
+          messageSize: '16',
+        });
+      }
+    } else {
+      hiddenLoad();
+      throw new Error(
+        'Sorry, there are no images matching your search query. Please try again!'
+      );
+    }
+  } catch (error) {
+    iziToast.error({
+      message: error.message,
+      position: 'topRight',
+      backgroundColor: '#EF4040',
+      theme: 'dark',
+      iconUrl: octagon,
+      maxWidth: 354,
+      messageSize: '16',
+    });
+  } finally {
+    loaderHide();
+    e.target.reset();
+  }
+}
+
+async function handleLoad() {
+  page += 1;
+  hiddenLoad();
+  loaderShow();
+
+  try {
+    const imagesData = await getImages(globalSearchCategory, page);
+    if (imagesData.hits.length) {
+      queryEl.galleryEl.insertAdjacentHTML(
+        'beforeend',
+        GalleryItem(imagesData.hits)
+      );
+      lightbox.refresh();
+
+      if (queryEl.galleryEl) {
+        smoothScrollByTwoCardHeights();
+      } else {
+        console.error("Element 'galleryItem' is not found.");
+      }
+      totalHits = Math.ceil(imagesData.totalHits / 15);
+      if (page < totalHits) {
+        visibilityLoad();
+      }
+    }
+  } catch (error) {
+    hiddenLoad();
+    iziToast.error({
+      message: "We're sorry, but you've reached the end of search results.",
+      backgroundColor: '#EF4040',
+      theme: 'dark',
+      maxWidth: 354,
+      messageSize: '16',
+    });
+  } finally {
+    loaderHide();
+  }
+}
 
 const lightbox = new SimpleLightbox('.gallery-link', {
   captionsData: 'alt',
   captionDelay: 250,
+  className: 'modal-image',
 });
 
-queryEl.searchForm.addEventListener(`submit`, e => {
-  e.preventDefault();
+function loaderShow() {
   queryEl.loader.style.display = 'inline-block';
+}
 
-  const searchQuery = e.target.elements.search.value.trim();
-  if (searchQuery === '') {
-    queryEl.galleryEl.innerHTML = '';
-    e.target.reset();
-    iziToast.warning({
-      title: `Warning`,
-      message: 'Search field cannot be empty!',
-      position: 'topRight',
-    });
-    return;
-  }
+function loaderHide() {
+  queryEl.loader.style.display = 'none';
+}
 
-  queryEl.galleryEl.innerHTML = '';
+function visibilityLoad() {
+  queryEl.loadBtn.classList.remove('hidden');
+}
 
-  getImages(searchQuery)
-    .then(imagesData => {
-      if (!imagesData.total) {
-        iziToast.error({
-          title: `Error`,
-          message:
-            'Sorry, there are no images matching your search query. Please try again!',
-          position: 'topRight',
-        });
-      }
-      queryEl.galleryEl.innerHTML = GalleryItem(imagesData.hits);
-      lightbox.refresh();
-    })
-    .catch(error => console.log(error))
-    .finally(() => {
-      queryEl.loader.style.display = 'none';
-      e.target.reset();
-    });
-});
+function hiddenLoad() {
+  queryEl.loadBtn.classList.add('hidden');
+}
+
+function getCardHeight() {
+  const card = queryEl.galleryEl.firstChild;
+  const cardRect = card.getBoundingClientRect();
+  return cardRect.height;
+}
+
+function smoothScrollByTwoCardHeights() {
+  const cardHeight = getCardHeight();
+  window.scrollBy({
+    left: 0,
+    top: cardHeight * 2,
+    behavior: 'smooth',
+  });
+}
